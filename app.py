9413c180e411626d271aee8cc9435fe34df51c48
developwithyashmhatre66666 <musicwithyashmhatre@gmail.com>
2026-05-11 00:18:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import uvicorn
 from Main import MainExecution
-import os
 
 app = FastAPI()
 
@@ -13,20 +11,13 @@ class ChatRequest(BaseModel):
 def home():
     return {"status": "Jagruti AI Running"}
 
-@app.get("/health")
-def health():
-    return {"status": "healthy"}
-
 @app.post("/chat")
 async def chat(req: ChatRequest):
+
     user_message = req.message
-    
-    # Call the AI function
+
     response = MainExecution(user_message)
 
-    return {"response": response}
-
-if __name__ == "__main__":
-    # Use the PORT environment variable if available, otherwise default to 10000
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    return {
+        "response": response
+    }
